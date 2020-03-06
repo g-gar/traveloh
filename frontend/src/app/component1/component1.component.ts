e3ee010f2cb0c8fff85975bf5c95a08ecdf98b22
ggar <g.gar.fern@hotmail.com>
2020-03-06 23:42:20 +0100

    Added visualizaztion of API response

--- frontend/src/app/component1/component1.component.ts
+++ frontend/src/app/component1/component1.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiSimpleMessageService } from '../service/api-simple-message.service';
+import $ from "jquery";
 
 @Component({
   selector: 'app-component1',
@@ -21,6 +22,7 @@ export class Component1Component implements OnInit {
       .subscribe(
        e => {
          console.log(e);
+         this.loadProgress(e);
        },
        error => {
          console.error(error);
@@ -29,4 +31,43 @@ export class Component1Component implements OnInit {
      )
    }
 
+   loadProgress(e: Object) {
+    console.log(e, typeof e)
+    e = e.results[0]
+    console.log(e)
+
+    let percentageToDegrees = (percentage: number) => {
+      return percentage / 100 * 360;
+    }
+
+    let formatNumber = (n: number) => {
+      return new Intl.NumberFormat('en-us', {minimumFractionDigits: 2}).format(n);
+    }
+
+    $(".row").each(function() {
+      $(this).find('.positive-compound').text(`${formatNumber(e.pos * 100)}%`)
+      $(this).find('.neutral-compound').text(`${formatNumber(e.neu * 100)}%`)
+      $(this).find('.negative-compound').text(`${formatNumber(e.neg * 100)}%`)
+    })
+
+    $(".progress").each(function() {
+      
+      let value = e.compound * 100;
+      var left = $(this).find('.progress-left .progress-bar');
+      var right = $(this).find('.progress-right .progress-bar');
+
+      $(this).find('.progress-value > .h2').text(`${value}`)
+      $(this).find('.progress-value > .h2').append('<sup class="small">%</sup>')
+
+      if (value > 0) {
+        if (value <= 50) {
+          right.css('transform', 'rotate(' + percentageToDegrees(value) + 'deg)')
+        } else {
+          right.css('transform', 'rotate(180deg)')
+          left.css('transform', 'rotate(' + percentageToDegrees(value - 50) + 'deg)')
+        }
+      }
+
+    })
+   }
 }
