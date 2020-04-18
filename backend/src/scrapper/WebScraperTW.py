5b1502331229a4059956bcffda269e2633aa94e8
Manuel <2155299@live.uem.es>
2020-04-18 18:49:46 +0200

    Scraper Vuelos a 10 aeropuetos diferentes
    Implementacion final del scraper para 10 aeropuertos

--- backend/src/scrapper/WebScraperTW.py
+++ backend/src/scrapper/WebScraperTW.py
@@ -12,6 +12,8 @@ import pandas as pd
 ##print 'Numero de Parametros: ', len(sys.argv)
 ##print 'Lista Argumentos: ', str(sys.argv)
 
+
+
 begin_date = dt.date(2020,3,15)
 end_date = dt.date(2020,3,16)
 limit = 500
@@ -19,3 +21,7 @@ lang = 'spanish'
 
 tweets = query_tweets('ovni', begindate = begin_date, enddate = end_date, limit = limit, lang = lang)
 df = pd.DataFrame(t.__dict__ for t in tweets)
+print(df.text)
+""" https://twitter.com/search?q=cornavirus&src=typed_query
+https://twitter.com/search?q=espa%C3%B1a&src=typed_query
+ """
