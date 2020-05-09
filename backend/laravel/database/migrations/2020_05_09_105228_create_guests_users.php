581b8f3cd4092829f58e7a6a11b53edf9bfcbde1
g-gar <g.gar.fern@hotmail.com>
2020-05-09 12:58:31 +0200

    created guest users migration

---
+++ backend/laravel/database/migrations/2020_05_09_105228_create_guests_users.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGuestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('guests', function (Blueprint $table) {
+            $table->integer('id')->unique()->nullable(0)->foreign('id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->string('request');
+            $table->string('response');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('guests');
+    }
+}
